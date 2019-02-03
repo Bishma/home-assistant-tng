c567367884a7f005ec0a1f86693c26e4f58f7037
Derek Rose <derek@idxbroker.com>
2019-02-02 22:37:29 -0800

    First go at AppDaemon Creates a date sensor that updates once per minute on the minute Sensor has readable date attrbutes to use in my alexa intent speech

---
+++ appdaemon/apps/utility_sensor.py
@@ -0,0 +1,27 @@
+import appdaemon.plugins.hass.hassapi as hass
+import datetime
+
+#
+# Hellow World App
+#
+# Args:
+#
+
+class Date(hass.Hass):
+
+    def initialize(self):
+        time = datetime.time(0, 0, 0)
+        self.handle = self.run_minutely(self.dateState, time)
+        self.log("Date sensor initialized.")
+
+    def dateState(self, kwargs):
+        now = datetime.datetime.now()
+        self.set_state("sensor.janet_utility_date", state = now.strftime('%Y-%m-%d %H:%M'), attributes = {
+            "r_dow": now.strftime('%A'),
+            "r_month": now.strftime('%B'),
+            "r_day": str(now.day) + self.suffix(now.day),
+            "year": now.strftime('%Y')
+        })
+
+    def suffix(self, d):
+        return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
