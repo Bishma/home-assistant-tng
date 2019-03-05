7bb9729fa1f2ea080aa313216e5591f0a45f003e
Derek Rose <derek@idxbroker.com>
2019-03-04 19:28:52 -0800

    Fix for emoji removal

--- appdaemon/apps/lunch-schedule.py
+++ appdaemon/apps/lunch-schedule.py
@@ -60,22 +60,6 @@ class Lunch(hass.Hass):
         })
     
     def stripEmoji(self, itemString):
-        emoji_pattern = re.compile("["
-            u"\U0001F600-\U0001F64F"  # emoticons
-            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-            u"\U0001F680-\U0001F6FF"  # transport & map symbols
-            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-            u"\U0001F1F2-\U0001F1F4"  # Macau flag
-            u"\U0001F1E6-\U0001F1FF"  # flags
-            u"\U0001F600-\U0001F64F"
-            u"\U00002702-\U000027B0"
-            u"\U000024C2-\U0001F251"
-            u"\U0001f926-\U0001f937"
-            u"\U0001F1F2"
-            u"\U0001F1F4"
-            u"\U0001F620"
-            u"\u200d"
-            u"\u2640-\u2642"
-            "]*", flags=re.UNICODE)
+        emoji_pattern = re.compile("[\U00010000-\U0010ffff]+", flags=re.UNICODE)
         
         return emoji_pattern.sub(r'', itemString)
