a144db6167ca406f41588c96757562c9b2c266fb
Derek Rose <derek@idxbroker.com>
2019-08-17 12:57:57 -0700

    Rewrite

--- www/custom-lovelace/list-item-card/list-item-card.js
+++ www/custom-lovelace/list-item-card/list-item-card.js
@@ -1,8 +1,67 @@
 class ListItem extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+    set hass(hass) {
+        if (!this.content) {
+            const card = document.createElement('ha-card');
+            if (this.config.title) {
+                // @TODO: support entity friendly_name if available and config.title is not
+                card.header = this.config.title;
+            }
+            this.content = document.createElement('div');
+            let style = document.createElement('style');
+            style.textContent = `
+                ha-card .header { var(--primary-text-color); }
+                .lc-wrapper {
+                    padding: 5px 5px 5px 20px;
+                    margin-bottom: 5px;
+                }
+                .lc-wrapper:last-child {
+                    padding-bottom: 18px;
+                }
+                .lc-wrapper .ha-icon {
+                    display: inline-block;
+                    height: 20px;
+                    width: 20px;
+                    margin-left: 5px;
+                    margin-right: 17px;
+                    color: var(--paper-item-icon-color);
+                }
+                .lc-item {
+                    display: inline-block;
+                    padding-left: 10px;
+                    padding-top: 2px;
+                }
+            `;
+            card.appendChild(this.content);
+            card.appendChild(style);
+            this.appendChild(card);
+        }
+
+        let listData = hass.states[this.config.entity].attributes[this.config.attribute];
+        // if the list isn't a list say so
+        if (!Array.isArray(listData)) {
+            listData = [
+                `The attribute '${this.config.attribute}'`,
+                ` of entity '${this.config.entity}'`,
+                ` is not a list.`
+            ];
+        }
+
+        // the number of items to display is the small of this.config.max_length or listData.length
+        const listLength = this.config.max_length < listData.length ? this.config.max_length : listData.length;
+        console.log(listLength)
+
+        let cardContent = "";
+        for (let i = 0; i < listLength; i++) {
+            cardContent += `
+                <div class='lc-wrapper'>
+                    <ha-icon class='ha-icon entity' ${this.config['list_icon']}></ha-icon>
+                    <div class='lc-item'>${listData[i]}</div>
+                </div>`;
+        }
+
+        this.content.innerHTML = cardContent;
     }
+    
     setConfig(config) {
         /**
          * The entity is required
@@ -12,115 +71,42 @@ class ListItem extends HTMLElement {
         }
 
         /**
-         * The attribute is required
+         * The attribute from the entity above is also required
          */
         if (!config.attribute) {
             throw new Error('Please define an attribute of the defined entity');
         }
 
-        const root = this.shadowRoot;
-
         /**
          * Ensure the config is an object
          */
-        const userConfig = Object.assign({}, config);
+        config = Object.assign({}, config);
 
         /**
          * The definable max list length needs to be an integer
          * If anything is amiss default to 4
          */
-        if (!userConfig.max_length || !Number.isInteger(userConfig.max_length)) {
-            userConfig.max_length = 4;
+        if (!config.max_length || !Number.isInteger(config.max_length)) {
+            config.max_length = 4;
         }
 
         /**
-         * Define the card as an object
+         * List item icons are optional
+         * @TODO: use the entity's icon if available and config.icon is not
          */
-        const card = document.createElement('ha-card');
-        if (userConfig.title) {
-            card.header = userConfig.title;
+        config['list_icon'] = '';
+        if (config.icon) {
+            config['list_icon'] = `icon='${config.icon}'`
+            delete config.icon;
         }
 
-        const content = document.createElement('div');
-        content.id = "primary-content"
-        const style = document.createElement('style');
-        style.textContent = `
-            ha-card {
-            }
-            .header { var(--primary-text-color); }
-            .lc-wrapper {
-				padding: 5px 5px 5px 20px;
-				margin-bottom: 5px;
-			}
-			.lc-wrapper:last-child {
-				padding-bottom: 18px;
-			}
-			.lc-wrapper .ha-icon {
-				display: inline-block;
-				height: 20px;
-				width: 20px;
-				margin-left: 5px;
-				margin-right: 17px;
-				color: var(--paper-item-icon-color);
-			}
-			.lc-item {
-				display: inline-block;
-				padding-left: 10px;
-				padding-top: 2px;
-			}
-        `;
-        card.appendChild(content);
-        card.appendChild(style);
-        root.appendChild(card);
-        this._config = userConfig;
+        this.config = config;
     }
-  
-    set hass(hass) {
-        const userConfig = this._config;
-        const root = this.shadowRoot;
-
-        const listData = hass.states[userConfig.entity].attributes[userConfig.attribute];
-        // if the list isn't a list say so
-        if (!Array.isArray(listData)) {
-            listData = [
-                `The attribute '${userConfig.attribute}'`,
-                ` of entity '${userConfig.entity}'`,
-                ` is not a list.`
-            ];
-        }
-
-        let iconAttr = '';
-        if (userConfig.icon) {
-            iconAttr = `icon='${userConfig.icon}'`
-        }
-
-        // the number of items to display is the small of userConfig.max_length or listData.length
-        const listLength = userConfig.max_length < listData.length ? userConfig.max_length : listData.length;
-  
-        if (listData !== this._listData) {
-            let cardContent = "";
-            for (let i = 0; i < listLength; i++) {
-                cardContent += `
-                    <div class='lc-wrapper'>
-                        <ha-icon class='ha-icon entity' ${iconAttr}></ha-icon>
-                        <div class='lc-item'>${listData[i]}</div>
-                    </div>`;
-            }
-
-            root.getElementById("primary-content").innerHTML = cardContent;
-
-            this._listData = listData
-        }
-        root.lastChild.hass = hass;
-    }
-
+    
+    // The height of your card. Home Assistant uses this to automatically
+    // distribute all cards over the available columns.
     getCardSize() {
-        if (Number.isInteger(this._config.max_length)) {
-            const cSize = 1 + Math.round(this._config.max_length / 4);
-        } else {
-            const cSize = 1;
-        }
-        return cSize;
+        return 1;
     }
 }
 
